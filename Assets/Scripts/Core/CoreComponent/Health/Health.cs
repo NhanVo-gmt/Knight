91e3dee9c2bd1f1f8f8e9d376ebc8ae6a86d085e
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2022-10-13 09:51:57 +0700

    adding hit animation
    adjusting the animation controller

--- Assets/Scripts/Core/CoreComponent/Health/Health.cs
+++ Assets/Scripts/Core/CoreComponent/Health/Health.cs
@@ -42,7 +42,7 @@ public class Health : CoreComponent
 
     void Recover()
     {
-        if (!isInvulnerable || isDie) return;
+        if (!IsInInvulnerabiltyTime() || isDie) return;
 
         if (hitTime + hitData.invulnerableTime < Time.time)
         {
@@ -53,7 +53,7 @@ public class Health : CoreComponent
     
     public void TakeDamage(AttackData attackData)
     {
-        if (healthData.health <= 0 || IsInInvulnerabiltyTime()) return;
+        if (healthData.health <= 0 || isInvulnerable) return;
 
         healthData.health -= attackData.damage;
 
@@ -82,8 +82,6 @@ public class Health : CoreComponent
 
     public bool IsInInvulnerabiltyTime()
     {
-        if (hitData == null) return false;
-
-        return hitTime + hitData.invulnerableTime < Time.time;
+        return isInvulnerable;
     }
 }
