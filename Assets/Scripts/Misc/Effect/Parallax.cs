4d64305cb59aea7284c39d7376e090854a7e904d
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2023-03-04 00:17:22 +1100

    new commit

--- Assets/Scripts/Misc/Effect/Parallax.cs
+++ Assets/Scripts/Misc/Effect/Parallax.cs
@@ -6,20 +6,22 @@ public class Parallax : MonoBehaviour
 {
     [SerializeField] float parralaxEffect;
     private float startPos;
+    
     private Camera cam;
-
+    private float camStartPosX;
     
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        camStartPosX = cam.transform.position.x;
         startPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dist = (cam.transform.position.x * parralaxEffect);
+        float dist = ((cam.transform.position.x - camStartPosX) * parralaxEffect);
         transform.position = new Vector2(startPos + dist, transform.position.y);
     }
 
