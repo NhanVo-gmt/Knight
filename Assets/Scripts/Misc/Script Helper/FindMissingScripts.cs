3ec790b6f86f6d071c509284f16e6e83d4c528f4
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2024-01-01 01:14:13 +0700

    adding find missing scripts

---
+++ Assets/Scripts/Misc/Script Helper/FindMissingScripts.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class FindMissingScripts
+{
+    [MenuItem("Knight/Missing Scripts/Find missing script in project")]
+    static void FindingMissingScriptsInProjectMenuItem()
+    {
+        string[] prefabPaths = AssetDatabase.GetAllAssetPaths()
+            .Where(path => path.EndsWith(".prefab", System.StringComparison.OrdinalIgnoreCase)).ToArray();
+        foreach (string path in prefabPaths)
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            foreach (Component component in prefab.GetComponentsInChildren<Component>())
+            {
+                if (component == null)
+                {
+                    Debug.LogError("Prefab found with missing scripts: " + path, prefab);
+                    break;
+                }
+            }
+        }
+    }
+
+    [MenuItem("Knight/Missing Scripts/Find missing scripts in scene")]
+    static void FindingMissingScriptsInScene()
+    {
+        foreach (GameObject gameObject in GameObject.FindObjectsOfType<GameObject>(true))
+        {
+            foreach (Component component in gameObject.GetComponentsInChildren<Component>())
+            {
+                if (component == null)
+                {
+                    Debug.LogError("Game Object found with missing scripts: " + gameObject.name, gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
